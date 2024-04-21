b0025af03258348a4b392e68267c9024fdc9a602
tinserrano <tinsonico@gmail.com>
2024-04-21 16:09:22 -0300

    se agrega google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import partytown from '@astrojs/partytown'
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://benditamiraiz.com',
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
